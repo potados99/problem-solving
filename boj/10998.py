9c5178f4be1dc7d4c1fe3e315a83db249182e8a7
potados99 <potados99@gmail.com>
2021-03-10 19:10:14 +0900

    10998, 11654번

---
+++ boj/10998.py
@@ -0,0 +1,7 @@
+# 10998번: A×B
+# https://www.acmicpc.net/problem/10998
+
+import sys; input = sys.stdin.readline
+
+a, b = map(int, input().split())
+print(a * b)
---
+++ boj/11654.py
@@ -0,0 +1,6 @@
+# 11654번: 아스키 코드
+# https://www.acmicpc.net/problem/11654
+
+import sys; input = sys.stdin.readline
+
+print(ord(input().rstrip()))
